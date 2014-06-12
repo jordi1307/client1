bd5f88619ac1f77dbd6478e2df2bf6eff2d8e342
jordi1307 <jluesma93@gmail.com>
2014-06-12 17:37:39 +0200

    ff

---
+++ Clien.py
@@ -0,0 +1,34 @@
+__author__ = 'jor'
+import
+class Clien:
+    nombre=""
+    apellido=""
+    dni=""
+    quenta=""
+    maill=""
+    telef=""
+    sexo=""
+    def __init__(self,nombre,apellido,dni,sexo="H",telef,quenta="",maill=""):
+        self.nombre=nombre
+        self.apellido=apellido
+        self.dni=dni
+        self.quenta=quenta
+        self.maill=maill
+        self.telef=telef
+        self.sexo=sexo
+    def getNombre(self):
+        return self.nombre
+
+    def getApellido(self):
+        return self.apellido
+    def getDni(self):
+        return self.dni
+    def getQuenta(self,dni,contact):
+    #if(maill)
+        return self.quenta
+    def getNombre(self):
+        return self.nombre
+    def getNombre(self):
+        return self.nombre
+    def getNombre(self):
+        return self.nombre
